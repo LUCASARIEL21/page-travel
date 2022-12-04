8fd66e9e1eddecabf9fa01d3f9895c97d01cf23f
LUCASARIEL21 <lukasbcunha@gmail.com>
2022-12-04 19:44:02 -0300

    atualizar page

--- src/pages/home/styles.jsx
+++ src/pages/home/styles.jsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 export const container = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
+  margin: 0 auto;
+  align-items: center;
   height: 100%;
   width: 100%;
   background: "white";
--- src/styles/App.jsx
+++ src/styles/App.jsx
@@ -1,19 +1,56 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: Georgia, 'Times New Roman', Times, serif;
-  }
-  * a{
-    text-decoration: none;
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-  * li{
-    list-style: none;
-  }
+html {
+  scroll-behavior: smooth;
+  scroll-padding-top: 75px;
+}
+
+body {
+  font-weight: 400;
+  overflow-x: hidden;
+  position: relative;
+  font-family: Georgia, 'Times New Roman', Times, serif;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  margin: 0;
+  padding: 0;
+  line-height: normal;
+}
+
+p, a, li, button, ul {
+  margin: 0;
+  padding: 0;
+  line-height: normal;
+  text-decoration: none;
+  list-style: none;
+}
+
+a:hover {
+  text-decoration: none;
+}
+
+img {
+  width: 100%;
+  height: auto;
+}
+
+button {
+  border: 0;
+  background-color: transparent;
+}
+
+input:focus, textarea:focus, select:focus {
+  outline: none;
+}
+
 `;
 
 export default GlobalStyle;
